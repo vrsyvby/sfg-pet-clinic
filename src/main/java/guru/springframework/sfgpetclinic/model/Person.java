d61027ca9ae91c673759f82ef882ff4dfe423e41
vrsyvby@gmail.com <3%@tatime>
2022-11-06 16:29:51 +0530

    Addng model classes to sfg-pet-clinic

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String firstName;
+    private String lastName;
+}
